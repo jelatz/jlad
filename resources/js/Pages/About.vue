d3215770d5e77c52e830822b13b67fbf036c6774
Jelatz <jelatz@Jelatzs-MacBook-Air.local>
2025-01-02 23:24:29 +0800

    initial commit

---
+++ resources/js/Pages/About.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <h1>This is an about page {{ user }}</h1>
+    </div>
+</template>
+
+<script setup>
+defineProps({
+    user: String,
+});
+</script>
---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+        <h1 class="text-5xl">Hello</h1>
+    </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
