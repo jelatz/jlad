7df27b8666549f04e1bd5192da47d485f0f0abfe
Jelatz <lanetejlad@gmail.com>
2025-01-11 22:45:53 +0800

    test

--- resources/js/Components/ProjectCard.vue
+++ resources/js/Components/ProjectCard.vue
@@ -2,6 +2,10 @@
     <div class="border-2 border-gray-200 rounded-lg p-4 w-[50rem] flex justify-left space-x-3">
         <img :src="saekyungAux" alt="">
         <p class="text-sm text-gray-500">Project Description</p>
+        <p class="text-sm text-gray-500">Project Description</p>
+        <p class="text-sm text-gray-500">Project Description</p>
+        <p class="text-sm text-gray-500">Project Description</p>
+        <p class="text-sm text-gray-500">Project Description</p>
     </div>
 </template>
 
