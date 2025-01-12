0ce5e0dc34b5294ee081f3edc6080de357b767d2
Jelatz <lanetejlad@gmail.com>
2025-01-12 20:55:32 +0800

    added condition on when to display the github icon and link.

--- resources/js/Components/ProjectCard.vue
+++ resources/js/Components/ProjectCard.vue
@@ -1,20 +1,33 @@
 <template>
-    <div class="rounded-lg p-4 w-[60rem] flex justify-left space-x-5">
-        <img :src="img" :alt="alt" class="object-contain w-96 rounded-md">
-        <div>
-            <h1 class="text-lg text-white font-bold">{{ title }}</h1>
-            <p class="text-sm mt-1">{{ description }}</p>
-        </div>
-        <div>
-            <a :href="githubLink" target="_blank">
-                <i class="fab fa-github text-2xl text-white"></i>
-            </a>
-        </div>
+    <div class="rounded-lg p-4 w-[60rem] flex justify-between text-left space-x-5">
+        <template v-if="opposite">
+            <div>
+                <h1 class="text-lg text-white font-bold">{{ title }}</h1>
+                <p class="text-sm mt-1">{{ description }}</p>
+                <div class="flex items-center space-x-2 mt-2">
+                    <a :href="githubLink" target="_blank" v-if="hasGithub">
+                        <i class="fab fa-github text-2xl text-white"></i>
+                    </a>
+                </div>
+            </div>
+            <img :src="img" :alt="alt" class="object-contain w-[30rem] rounded-md">
+        </template>
+        <template v-else>
+            <img :src="img" :alt="alt" class="object-contain w-[30rem] rounded-md">
+            <div>
+                <h1 class="text-lg text-white font-bold">{{ title }}</h1>
+                <p class="text-sm mt-1">{{ description }}</p>
+                <div class="flex items-center space-x-2 mt-2">
+                    <a :href="githubLink" target="_blank" v-if="hasGithub">
+                        <i class="fab fa-github text-2xl text-white"></i>
+                    </a>
+                </div>
+            </div>
+        </template>
     </div>
 </template>
 
 <script setup>
-
 defineProps({
     img: {
         type: String,
@@ -34,6 +47,12 @@ defineProps({
     },
     githubLink: {
         type: String,
+    },
+    opposite: {
+        type: Boolean,
+    },
+    hasGithub: {
+        type: Boolean
     }
 })
 </script>
--- resources/js/Pages/Projects.vue
+++ resources/js/Pages/Projects.vue
@@ -1,8 +1,6 @@
 <template>
     <div class="flex items-center justify-center w-fit px-5 py-1 bg-[#353b44] rounded-full ml-5">
         <Link :href="route('home')">
-        <!-- <div class="">
-        </div> -->
         <i class="fa-solid fa-arrow-left"></i>
         <span class="ml-2">Back Home</span>
         </Link>
@@ -10,9 +8,10 @@
 
     <div class="mt-10 container">
         <h1 class="text-center text-5xl font-bold">My work</h1>
-        <div class="mt-5 flex justify-between items-center flex-col">
+        <div class="mt-5 flex justify-between items-center flex-col space-y-10">
             <ProjectCard v-for="(project, index) in projects" :key="index" :img="project.img" :alt="project.alt"
-                :title="project.title" :description="project.description" :githubLink="project.github" />
+                :title="project.title" :description="project.description" :githubLink="project.github"
+                :opposite="project.opposite" :hasGithub="project.hasGithub" />
         </div>
     </div>
 </template>
@@ -27,44 +26,49 @@ const projects = ref([
         alt: 'Saekyung Auxilium',
         title: 'Saekyung Auxilium',
         description: 'This is my capstone project. A web application that helps the Saekyung Condominium to manage their residents and visitor concerns.',
-        github: 'https://github.com/jelatz/Saekyung-Auxilium'
+        github: 'https://github.com/jelatz/Saekyung-Auxilium',
+        hasGithub: true
     },
     {
         img: '/images/projects/dashboard.png',
         alt: 'Healteeth',
         title: 'Healteeth',
         description: 'This is a side project',
-        github: 'https://github.com/jelatz/healteeth'
+        github: 'https://github.com/jelatz/healteeth',
+        hasGithub: true,
+        opposite: true
     },
     {
         img: '/images/projects/enrichment-unlimited.jpg',
         alt: 'Enrichment Unlimited',
         title: 'Enrichment Unlimited',
-        description: 'Project created from previous company using wordpress'
+        description: 'This is my capstone project. A web application that helps the Saekyung Condominium to manage their residents and visitor concerns.',
     },
     {
         img: '/images/projects/integrity-support-services.jpg',
         alt: 'Integrity Support Services',
         title: 'Integrity Support Services',
-        description: 'Project created from previous company using wordpress'
+        description: 'Project created from previous company using wordpress. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi.',
+        opposite: true
     },
     {
         img: '/images/projects/lyons-global.jpg',
         alt: 'Lyons Global',
         title: 'Lyons Global',
-        description: 'Freelance Project'
+        description: 'Freelance Project lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi.',
     },
     {
         img: '/images/projects/nj-leaf.jpg',
         alt: 'NJ Leaf',
         title: 'NJ Leaf',
-        description: 'Project created from previous company using wordpress'
+        description: 'Project created from previous company using wordpress lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi.',
+        opposite: true
     },
     {
         img: '/images/projects/one-heart-medical-staffing.jpg',
         alt: 'One Heart Medical Staffing',
         title: 'One Heart Medical Staffing',
-        description: 'Project created from previous company using wordpress'
+        description: 'Project created from previous company using wordpress lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi.',
     }
 ])
 </script>
